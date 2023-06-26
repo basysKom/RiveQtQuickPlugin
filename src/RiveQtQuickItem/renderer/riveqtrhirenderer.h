bf2c12fe6931404bc56ca953c6514c1c80028c7b
Jonas Kalinka <jonas.kalinka@basyskom.com>
2023-06-26 13:28:26 +0200

    removed spaces

--- src/RiveQtQuickItem/renderer/riveqtrhirenderer.h
+++ src/RiveQtQuickItem/renderer/riveqtrhirenderer.h
@@ -96,7 +96,7 @@ class RiveQtRhiRenderer : public rive::Renderer
 public:
     RiveQtRhiRenderer(QQuickItem *item);
     virtual ~RiveQtRhiRenderer();
-    
+
     void setFillMode(const RiveRenderSettings::FillMode fillMode) { m_fillMode = fillMode; };
 
     void save() override;
@@ -135,6 +135,6 @@ private:
 
     QSize m_artboardSize;
     QRectF m_viewportRect;
-    
+
     RiveRenderSettings::FillMode m_fillMode;
 };
