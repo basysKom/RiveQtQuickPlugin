dc0d7736a69549b997278891d5e4759a6806cb26
Jeremias Bosch <jeremias.bosch@gmail.com>
2023-07-03 22:14:34 +0200

    Correct gradient drawing issue in qt5 opengl

--- src/RiveQtQuickItem/shaders/path_vertex_shader.glsl
+++ src/RiveQtQuickItem/shaders/path_vertex_shader.glsl
@@ -15,58 +15,18 @@ uniform vec2 u_gradientStart;
 uniform vec2 u_gradientEnd;
 
 uniform vec2 u_gradientCenter;
-uniform vec2 u_gradientFocalPoint;
 uniform float u_gradientRadius;
 uniform vec2 u_viewportDimensions;
 
 uniform mat4 u_transform;
 uniform mat4 u_projection;
 
-out float v_gradientCoord;
-out vec2 v_gradientStart;
-out vec2 v_gradientEnd;
-
 out vec2 v_texCoord;
+out vec2 originalVertex;
 
 void main() {
-    if (u_gradientType == 0) { // Linear gradient
-        vec2 gradientStart = u_gradientStart;
-        vec2 gradientEnd = u_gradientEnd;
-
-        vec2 gradientDir = u_gradientEnd - u_gradientStart;
-        float gradientLength = length(gradientDir);
-        vec2 normalizedGradientDir = gradientDir / gradientLength;
-
-        if (gradientLength > 0.0) {
-            vec2 posDir = a_position - u_gradientStart;
-            float posCoord = dot(posDir, normalizedGradientDir);
-            v_gradientCoord = posCoord / gradientLength;
-            v_gradientCoord = clamp(v_gradientCoord, 0.0, 1.0);
-        } else {
-            v_gradientCoord = 0.0;
-        }
-
-        v_gradientStart = gradientStart;
-        v_gradientEnd = gradientEnd;
-    } else { // Radial gradient
-        vec2 dir = a_position - u_gradientCenter;
-        vec2 focalDir = u_gradientFocalPoint - u_gradientCenter;
-        float dist = length(dir);
-        vec2 normalizedDir = dir / dist;
-        vec2 normalizedFocalDir = focalDir / u_gradientRadius;
-        float dotProduct = dot(normalizedDir, normalizedFocalDir);
-        float angle = acos(dotProduct);
-        float normalizedAngle = angle / (2.0 * 3.14159);
-
-        v_gradientCoord = dist / u_gradientRadius;
-        v_gradientCoord = clamp(v_gradientCoord, 0.0, 1.0) * cos(normalizedAngle);
-
-        v_gradientStart = u_gradientCenter;
-        v_gradientEnd = u_gradientFocalPoint;
-    }
-
     v_texCoord = a_texCoord;
-    //v_texCoord = vec2(gl_Position.x, gl_Position.y);// / vec2(u_viewportDimensions);
+    originalVertex = a_position;
     gl_Position = u_projection * u_transform * vec4(a_position, 0.0, 1.0);
 }
 
