d9da8cf783d3e2cb794ed190cfd5d9668792cc07
Jeremias Bosch <jeremias.bosch@gmail.com>
2023-07-03 22:14:34 +0200

    Correct gradient drawing issue in qt5 opengl

--- src/RiveQtQuickItem/shaders/path_fragement_shader.glsl
+++ src/RiveQtQuickItem/shaders/path_fragement_shader.glsl
@@ -10,33 +10,39 @@ uniform vec4 u_color;
 uniform bool u_useGradient;
 uniform int u_gradientType;
 
+uniform vec2 u_gradientStart;
+uniform vec2 u_gradientEnd;
+
 uniform int u_numStops;
 uniform vec2 u_stopPositions[20]; //normalized!
 uniform vec4 u_stopColors[20];
 
 uniform vec2 u_viewportDimensions;
+uniform vec2 u_gradientCenter;
+uniform float u_gradientRadius;
+
 in vec2 v_texCoord;
 
-in float v_gradientCoord;
-in vec2 v_gradientStart;
-in vec2 v_gradientEnd;
+in vec2 originalVertex;
 out vec4 outColor;
 
 uniform sampler2D u_alphaMaskTexture;
 
 uniform bool u_useAlphaMask;
 
-// Gradient related functions
-vec4 getGradientColor(float gradientCoord) {
+vec4 getGradientColor( float gradientCoord) {
     vec4 gradientColor = u_stopColors[0];
+
+    if (gradientCoord >= u_stopPositions[u_numStops - 1].x) {
+        return u_stopColors[u_numStops - 1];
+    }
 
     for (int i = 1; i < u_numStops; ++i) {
         if (gradientCoord <= u_stopPositions[i].x) {
-            gradientColor = mix(u_stopColors[i - 1], u_stopColors[i], smoothstep(u_stopPositions[i - 1].x, u_stopPositions[i].x, v_gradientCoord));
+            gradientColor = mix(u_stopColors[i - 1], u_stopColors[i], smoothstep(u_stopPositions[i - 1].x, u_stopPositions[i].x, gradientCoord));
             break;
         }
     }
-
     return gradientColor;
 }
 
@@ -46,14 +52,23 @@ void main() {
 
     vec4 baseColor = u_color;
     if (u_useGradient) {
-        vec4 gradientColor = getGradientColor(v_gradientCoord);
-        baseColor = gradientColor;
+        float gradientCoord;
+        if (u_gradientType == 0) { // Linear gradient
+            vec2 dir = originalVertex - u_gradientStart;
+            vec2 gradientDir = u_gradientEnd - u_gradientStart;
+            gradientCoord = dot(dir, gradientDir) / dot(gradientDir, gradientDir);
+        } else { // Radial gradient
+            vec2 dir = originalVertex - u_gradientCenter;
+            gradientCoord = length(dir) / u_gradientRadius;
+        }
+        gradientCoord = clamp(gradientCoord, 0.0, 1.0);
+        baseColor = getGradientColor(gradientCoord);
     }
 
-   if (u_useAlphaMask) {
+    if (u_useAlphaMask) {
        vec4 maskColor = texture(u_alphaMaskTexture, normalizedFragCoord);
        outColor = vec4(baseColor.rgb, baseColor.a * maskColor.a);
-   } else {
+    } else {
        outColor = baseColor;
     }
 }
